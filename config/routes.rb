3fc9f5e3fb759b7d775a3b8124a7b267fd19b46e
Dana <peppoya@gmail.com>
2016-11-26 00:10:13 +0000

    Comments for individual home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  resources :homes
+  
+  
+  resources :homes do
+    resources :comments
+  end
+
+  resources :comments do
+    resources :comments
+  end
 end
