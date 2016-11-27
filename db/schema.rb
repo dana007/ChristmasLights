61426119d034976bbdc9cdbea7e9ab93addf4db4
Nipun Monga <nipunmnga@gmail.com>
2016-11-27 16:54:57 -0500

    Adding remaining filters. My Homes page only shows homes for the logged in user.

--- db/schema.rb
+++ db/schema.rb
@@ -46,12 +46,8 @@ ActiveRecord::Schema.define(version: 20161127002612) do
     t.string   "title"
     t.integer  "rating"
     t.integer  "user_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.string   "thumbnail_file_name"
-    t.string   "thumbnail_content_type"
-    t.integer  "thumbnail_file_size"
-    t.datetime "thumbnail_updated_at"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "street_address"
     t.string   "city"
     t.string   "state"
