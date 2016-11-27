5fde029a78c2600d2e20d5859e9296acc2e25197
Dana <peppoya@gmail.com>
2016-11-27 00:48:46 +0000

    more filter categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 
 #homes = Home.create([{address: 'Vienna' }, { address: 'Fairfax' }, { address: 'Leesburg' }])
 homes = Home.create([{title: 'House 4',  state: 'VA', city: 'Vienna', rating: 5 }, 
-{title: 'House 5',  state: 'VA',city: 'Fairfax', rating: 2 }, {title: 'House 6',  state: 'VA', city: 'Leesburg', rating: 4 }])
+{title: 'House 5',  state: 'VA',city: 'Fairfax', rating: 2 }, {title: 'House 6',  state: 'MD', city: 'Chevy Chase', rating: 4 }])
 
 #Home.create(title: 'House 1', state: 'VA', city: 'McLean', rating: 5)
 #Home.create(title: 'House 2', state: 'MD',city: 'Chevy Chase', rating: 4)
