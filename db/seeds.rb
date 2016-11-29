d41730a7e64e5907a14102307c562ace8a1f5e10
Dana <peppoya@gmail.com>
2016-11-29 03:10:36 +0000

    Individual home page displays address and comment history

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,16 @@
 # homes = Home.create([{title: 'House 4',  state: 'VA', city: 'Vienna', rating: 5 },
 # {title: 'House 5',  state: 'VA',city: 'Fairfax', rating: 2 }, {title: 'House 6',  state: 'MD', city: 'Chevy Chase', rating: 4 }])
 
-Home.create(title: 'House 1', city: 'McLean', state: 'MD', zip_code: '12345', rating: 5)
-Home.create(title: 'House 2', city: 'Ashburn', state: 'VA', zip_code: '12345', rating: 4)
-Home.create(title: 'House 3', city: 'Fairfax', state: 'VA', zip_code: '12345', rating: 2)
+Home.create(title: 'House 1', street_address: "1234 Main St.", city: 'McLean', state: 'MD', zip_code: '12345', rating: 5)
+Home.create(title: 'House 2', street_address: "5678 Main St.", city: 'Ashburn', state: 'VA', zip_code: '12345', rating: 4)
+Home.create(title: 'House 3', street_address: "007 Bond St.", city: 'Fairfax', state: 'VA', zip_code: '12345', rating: 2)
+
+Comment.create(home_id: '30', content: 'First comment for home_id =12')
+Comment.create(home_id: '30', content: 'Another comment for home_id =12')
+Comment.create(home_id: '30', content: 'Last comment for home_id =12')
+
+Comment.create(home_id: '29', content: 'Test comment for home_id =11')
+Comment.create(home_id: '29', content: 'Another test comment for home_id =11')
+
+Comment.create(home_id: '28', content: 'Random comment for home_id =10')
+Comment.create(home_id: '28', content: 'The comment for home_id =10')
